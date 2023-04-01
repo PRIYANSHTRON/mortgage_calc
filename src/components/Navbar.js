6d31dd29f9b23f992e204f9dea99e98b65b66c1d
PRIYANSHTRON <priyanshrusia2002@gmail.com>
2023-04-02 02:17:07 +0530

    styled Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import { Container } from '@mui/material';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <AppBar position="static">
+        <Toolbar>
+          
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+           <Container> Mortgage Calculator</Container>
+          </Typography>
+         
+        </Toolbar>
+      </AppBar>
   )
 }
 
